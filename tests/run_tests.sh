bf1fb9ee23cc0fd5419cc8b5089265bf47acc48b
Frederic Stark <frederic.stark@gmail.com>
2025-11-23 21:50:59 +0100

    Add proper test execution with non-zero exit on failure

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
+set -e  # Exit on any error
+
 ../flimmaker test_01.mp4 --flim test_01.flim
 
 gunzip -c test_01.flim.gz > test_01.flim.decompressed
 if cmp -s test_01.flim test_01.flim.decompressed; then
 	echo "TEST PASSED"
+	rm -f test_01.flim.decompressed
+	rm -f test_01.flim
+	exit 0
 else
 	echo "TEST FAILED"
+	rm -f test_01.flim.decompressed
+	rm -f test_01.flim
+	exit 1
 fi
-rm -f test_01.flim.decompressed
-rm -f test_01.flim
